326fea9af769fdf6f667c2bd4225a509527a3119
Steven Bui <bui.stvn@gmail.com>
2021-12-09 10:59:58 -0800

    Complete day01 part two

--- 2021/day01/day01.py
+++ 2021/day01/day01.py
@@ -14,4 +14,27 @@ def part_one():
 
     print("Part One:", count)
 
+def part_two():
+    count = 0
+    array = []
+
+    f = open("input.txt", "r")
+    lines = f.readlines()
+
+    for line in lines:
+        array.append(int(line))
+
+    current_window = array[0] + array[1] + array[2]
+
+    for i in range(1, len(array) - 2):
+        next_window = array[i] + array[i + 1] + array[i + 2]
+
+        if next_window > current_window:
+            count += 1
+
+        current_window = next_window
+
+    print("Part Two:", count)
+
 part_one()
+part_two()
