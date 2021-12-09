4ef38658f1127d9c33c25b76175019c46c302e73
Steven Bui <bui.stvn@gmail.com>
2021-12-09 11:27:38 -0800

    Complete day02 part two

--- 2021/day02/day02.py
+++ 2021/day02/day02.py
@@ -22,4 +22,31 @@ def part_one():
 
     print("Part One:", horizontal_position * depth)
 
+def part_two():
+    horizontal_position = 0
+    depth = 0
+    aim = 0
+    directions = []
+    units = []
+
+    f = open("input.txt", "r")
+    lines = f.readlines()
+
+    for line in lines:
+        command = line.split()
+        directions.append(command[0])
+        units.append(int(command[1]))
+
+    for i in range(0, len(lines)):
+        if directions[i] == "forward":
+            horizontal_position += units[i]
+            depth += (aim * units[i])
+        elif directions[i] == "down":
+            aim += units[i]
+        elif directions[i] == "up":
+            aim -= units[i]
+
+    print("Part Two:", horizontal_position * depth)
+
 part_one()
+part_two()
